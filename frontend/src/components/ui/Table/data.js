fca615013a61ff6382fc61cdf465e7b9ccea49d3
liordesta <liord013@gmail.com>
2023-05-02 11:54:23 +0300

    value did not match 250

--- frontend/src/components/ui/Table/data.js
+++ frontend/src/components/ui/Table/data.js
@@ -12,6 +12,6 @@ export const rowsPerPage = [
   { id: '1', name: '10', value: 10 },
   { id: '2', name: '50', value: 50 },
   { id: '3', name: '100', value: 100 },
-  { id: '4', name: '250', value: 200 },
+  { id: '4', name: '250', value: 250 },
   { id: '5', name: '500', value: 500 },
 ];
